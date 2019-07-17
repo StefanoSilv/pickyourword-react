5c68b50eeab348f57a9195a3a2592a13f870e624
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-17 05:40:14 +0200

    Logout component created

---
+++ src/Logout.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import './Logout.css';
+
+class Logout extends Component {
+	// Data
+	state = {
+
+	}
+	// Functions
+	//Function that remove the local storage and redirect you to the loginpage
+	logout = (e) => {
+		e.preventDefault()
+		localStorage.removeItem('token')
+		window.location.href = '/login'
+	}
+
+	// Render
+	render() {
+		return (
+			<div className="text-center">
+				<button  onClick={ (e) => this.logout(e)}
+				className="btn btn-secondary">Log-out</button>
+			</div>
+		)
+	}
+}
+
+export default Logout;
