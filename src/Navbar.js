0caf21d787eab696eb4e038acd59ed7d983d45c9
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-18 11:53:50 +0200

    Navbar functionalities fixed and related to authentication

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,15 +7,32 @@ class Navbar extends Component {
 	// Data
 
 	// Functions
+	//redirect to Login page
+	loginRedirect = () => {
+		window.location.href = '/login'
+	}
+
+	//redirect to Signup page
+	signupRedirect = () => {
+		window.location.href = '/signup'
+	}
 
 	// Render
 	render() {
 		return (
-			<div id="navbar" class="row">
+			<div id="navbar" className="row">
 				<div id="game-logo">Game logo</div>
 				<div id="points-level">Points & level</div>
 				<div id="trophy">Last trophy</div>
-				<User />
+				{ this.props.checkAuth() ? <User /> :
+					<div id="user-navbar">
+						<div id="logged-in">
+							<small>Do you have an account?</small>
+							<p onClick={() => this.loginRedirect()}>Log-in</p>
+							<small>Otherwise... </small><span onClick={() => this.signupRedirect()}>Sign-up</span>
+						</div>
+					</div>
+				}
 			</div>
 
 		)
