b49963419963e5e5678491d29b5585fe9648dc87
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-18 11:53:20 +0200

    Authorization system added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,34 @@ import './App.css';
 import Navbar from './Navbar'
 import Bottonbar from './Bottonbar'
 import Content from './Content'
+import axios from 'axios'
 
 
 
 class App extends Component {
 	// Data
+	state = {
+		user : []
+	}
 
 	// Functions
+
+
+	componentWillMount() {
+		axios.get(`${process.env.REACT_APP_API}/api/me`).then((res) => {
+
+			console.log('res', res.data);
+	}).catch( (err) => {
+		console.log(err);
+	})
+}
 
 	// Render
 	render() {
 		return (
-			<div id="main-container" class="container-fluid h-100">
-				<Navbar />
-				<Content />
+			<div id="main-container" className="container-fluid h-100">
+				<Navbar checkAuth={this.props.checkAuth}/>
+				<Content checkAuth={this.props.checkAuth} />
 				<Bottonbar />
 			</div>
 		)
