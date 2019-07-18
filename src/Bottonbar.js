ca8c20274708e9851958861c57a2396d47ca6d35
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-18 05:55:21 +0200

    Bottonbar component created

---
+++ src/Bottonbar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Bottonbar.css';
+
+
+class Bottonbar extends Component {
+	// Data
+
+	// Functions
+
+	// Render
+	render() {
+		return (
+		)
+	}
+}
+
+export default Bottonbar;
