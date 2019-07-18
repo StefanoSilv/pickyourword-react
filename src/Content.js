075dcb51f970adbce9c09f2f2d56e2b3cf301cb9
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-18 05:53:43 +0200

    Content component created

---
+++ src/Content.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Content.css';
+
+
+class Content extends Component {
+	// Data
+
+	// Functions
+
+	// Render
+	render() {
+		return (
+		)
+	}
+}
+
+export default Content;
