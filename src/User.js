0f1862836668f6ef09e7dc906362373dd3551d59
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-18 10:52:31 +0200

    Dropdown menu added on click

--- src/User.js
+++ src/User.js
@@ -8,16 +8,24 @@ class User extends Component {
 	// Data
 
 	// Functions
+dropdownMenu = () => {
+var x = document.getElementById("user-unordered-list")
+	if (x.className.indexOf("show") == -1) {
+	x.className += "show";
+	} else {
+	x.className = x.className.replace("show", "")
+	}
+}
 
 	// Render
 	render() {
 		return (
 			<div id="user-navbar">
 				<div id="logged-in">
-					<button id="get-premium" type="button" class="btn btn-outline-warning">Get Premium</button>
-					<p id="user-account">Your account</p>
-						<ul>
-							<li>Nickname: randomnickname</li>
+					<button id="get-premium" type="button" className="btn btn-outline-warning">Get Premium</button>
+					<p id="user-account" onClick={() => this.dropdownMenu()}>Your account</p>
+						<ul id="user-unordered-list">
+							<li id="nickname-navbar">Nickname: randomnickname</li>
 							<li>email: randomemail</li>
 							<li>level: 10000000000</li>
 							<li>points:100000000000</li>
