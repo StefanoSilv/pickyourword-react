bfe156d9144415bf0b25b3b46be5bc9f8362f8eb
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-19 05:07:31 +0200

    Navbar fixed with trophy, points and level

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 import './Navbar.css';
 import User from './User'
+import axios from 'axios'
 
 
 class Navbar extends Component {
 	// Data
+	state = {
+		me : {
+			points:0,
+			level:'Guest',
+			trophy: ['Guest']
+		}
+	}
 
 	// Functions
 	//redirect to Login page
@@ -17,14 +25,31 @@ class Navbar extends Component {
 		window.location.href = '/signup'
 	}
 
+	//take the info from the logged user
+	componentWillMount() {
+		axios.get(`${process.env.REACT_APP_API}/api/me`, {headers: {
+			Authorization: `Bearer ${localStorage.getItem('token')}`
+		}}).then( (res) => {
+			let user = res.data
+			console.log('res.data in the function' ,res.data);
+			this.setState({me : user})
+			console.log('user', this.state.me);
+		}).catch( (err) => {
+		console.log(err);
+	})
+}
+
 	// Render
 	render() {
 		return (
 			<div id="navbar" className="row">
 				<div id="game-logo">Game logo</div>
-				<div id="points-level">Points & level</div>
+				<div id="points-level" >
+					<div id="points">{this.state.me.points}</div>
+					<div id="level">{this.state.me.level.toUpperCase()}</div>
+				</div>
 				<div id="trophy">Last trophy</div>
-				{ this.props.checkAuth() ? <User /> :
+				{ this.props.checkAuth() ? <User me={this.state.me} key={this.state.me._id}/> :
 					<div id="user-navbar">
 						<div id="logged-in">
 							<small>Do you have an account?</small>
