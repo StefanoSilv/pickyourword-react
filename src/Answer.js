f8042d07076be71a78c6d7ba7ebc0531163ead84
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-19 09:05:56 +0200

    change the text and store it in the state of answer component

--- src/Answer.js
+++ src/Answer.js
@@ -1,23 +1,57 @@
 import React, {Component} from 'react';
 import './Answer.css';
+import axios from 'axios'
 
 
 class Answer extends Component {
 	// Data
+	state = {
+		text : ''
+	}
 
 	// Functions
+	changeText = (e) => {
+		this.setState({
+			text: e.target.value
+		})
+	}
+
+	clearAnswer = () => {
+		this.setState({
+			text: ''
+		})
+	}
+
+	createAnswer = (e, text) => {
+		e.preventDefault()
+		axios.post(
+			`${process.env.REACT_APP_API}/api/checkAnswer`,
+			text,
+			{headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`
+			}}
+		).then((res) => {
+			console.log(res.data)
+		}).catch((err) => {
+			console.log('err', err)
+		})
+	}
 
 	// Render
 	render() {
 		return (
-			<div id="answer-box">
+			<form onSubmit={(e) => {
+					this.createAnswer(e, this.state.text); this.clearAnswer()}
+				} id="answer-box">
 				<div className="input-group mb-3">
-					<input type="text" className="form-control" placeholder="Pick your word" />
+					<input onChange={(e) => this.changeText(e)} value={this.state.text}
+					 type="text" className="form-control"
+					 placeholder="Pick your word" />
 					<div id="confirm-button" className="input-group-append">
 						<button className="btn btn-outline-secondary" type="button">Confirm!</button>
 					</div>
 				</div>
-			</div>
+			</form>
 		)
 	}
 }
