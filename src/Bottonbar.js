a88f025b45bc913984f666bee1846dbdf73b1492
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-30 07:21:52 +0200

    botton bar changed

--- src/Bottonbar.js
+++ src/Bottonbar.js
@@ -12,18 +12,16 @@ class Bottonbar extends Component {
 		return (
 			<div id="bottonbar" className="row align-items-center">
 				<div id="info" className="col-6">
-					<p>App developed by Stefano Silvestrini</p>
-					<p>Tortuga Coders</p>
-					<p>Koh Samui, Thailand</p>
+					<p id="developer">App developed by Stefano Silvestrini</p>
+					<p>Rules</p>
 				</div>
-				<div id="sharing" className="col-6"><div className="fb-share-button"
-				data-href="https://pickyourword.herokuapp.com"
-				data-layout="button_count" data-size="small">
-				<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
-				className="fb-xfbml-parse-ignore">แชร์</a></div></div>
-				<a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site http://www.website.com."
-   title="Share by Email">Hello
-		</a>
+					<div id="sharing" className="col-6"><div className="fb-share-button"
+					data-href="https://pickyourword.herokuapp.com"
+					data-layout="button_count" data-size="small">
+					<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
+					className="fb-xfbml-parse-ignore">แชร์</a></div>
+					
+				</div>
 			</div>
 		)
 	}
